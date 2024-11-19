1cbe62e7bbb0cefd1d7f64ad69a8a059719705d9
Benny254 <asshimbenny01@gmail.com>
2024-11-19 08:50:42 +0000

    update

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""Class User for ORM"""
-
-from sqlalchemy import Column, Integer, String
+""" SQLAlchemy model for User
+"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
 Base = declarative_base()
 
 
 class User(Base):
-        """Representation of a user """
+        """ User class """
             __tablename__ = 'users'
                 id = Column(Integer, primary_key=True)
                     email = Column(String(250), nullable=False)
